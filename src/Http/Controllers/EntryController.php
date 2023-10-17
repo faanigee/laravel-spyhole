ae365d3c179e9ee66317d590f334c4cf2c98d48e
Irfan Ahmad <irfanahmad@msn.com>
2023-10-17 15:46:22 +0500

    Update EntryController.php

--- src/Http/Controllers/EntryController.php
+++ src/Http/Controllers/EntryController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Kalizi\LaravelSpyhole\Http\Controllers;
+namespace Faanigee\LaravelSpyhole\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller;
